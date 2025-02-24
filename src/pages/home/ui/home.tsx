b51e5b7d7d77595a1bb56db31844e8a787db5ebd
Jigoooo <jiwook97@gmail.com>
2025-02-25 08:30:11 +0900

    init

--- src/pages/home/ui/home.tsx
+++ src/pages/home/ui/home.tsx
@@ -8,7 +8,7 @@ export function Home() {
 
   return (
     <MainLayout headerTitle={menuState.selectedMenu.name}>
-      <Typography>Contents</Typography>
+      <Typography>123</Typography>
     </MainLayout>
   );
 }
